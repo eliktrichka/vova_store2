4b529ff062fe6406499768d96c5c0fcf7d2b0163
eliktrichka <wowaci@yandex.ru>
2018-11-11 12:32:38 +0200

    Delete yevheniy_upload.php

--- yevheniy_upload.php
+++
@@ -1,9 +0,0 @@
-<!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
-<form enctype="multipart/form-data" action="yevheniy2.php" method="POST">
-    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-    <!-- Название элемента input определяет имя в массиве $_FILES -->
-    Отправить этот файл: <input name="userfile" type="file" />
-    <input type="submit" value="Отправить файл" />
-</form>
-
